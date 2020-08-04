75f6a6b5106f48fcda4f2fff6037869939db4c4d
Cosme927 <67229630+Cosme927@users.noreply.github.com>
2020-08-04 17:51:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#Add The Theme：luci-
+git clone https://github.com/P3TERX/xxx package/xxx
